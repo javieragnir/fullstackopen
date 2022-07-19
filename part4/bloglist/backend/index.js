a80e627b1b3fc7521c3913399b78a5ccdf6d8ed6
javier <jjr.agnir@gmail.com>
2022-07-19 10:58:39 +0800

    Make modules for logger, config, and blog mongoose model

--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -1,22 +1,26 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+//
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
+//
 
-const Blog = mongoose.model('Blog', blogSchema)
+const MONGODB_URI = config.MONGODB_URI
 
-const MONGODB_URI = process.env.MONGODB_URI
+logger.info('connecting to', MONGODB_URI)
 
 mongoose.connect(MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
@@ -41,5 +45,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    logger.info(`Server running on port ${PORT}`)
 })
