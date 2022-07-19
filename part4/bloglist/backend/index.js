8bc997881635042420798a004477d3539c8c534b
javier <jjr.agnir@gmail.com>
2022-07-19 11:08:35 +0800

    Make controllers module

--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const Blog = require('./models/blog')
+const notesRouter = require('./controllers/blogs')
 //
 
 const MONGODB_URI = config.MONGODB_URI
@@ -25,7 +26,7 @@ mongoose.connect(MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+/* app.get('/api/blogs', (request, response) => {
     Blog
         .find({})
         .then(blogs => {
@@ -41,7 +42,9 @@ app.post('/api/blogs', (request, response) => {
         .then(result => {
             response.status(201).json(result)
         })
-})
+}) */
+
+app.use('/api/blogs', notesRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
