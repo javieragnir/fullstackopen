c47aebbd72ec15ae0b842e22bef04885c5c8a60d
javier <jjr.agnir@gmail.com>
2022-07-22 09:50:20 +0800

    complete up to 4.18

--- part4/bloglist/bloglist-backend/controllers/blogs.js
+++ part4/bloglist/bloglist-backend/controllers/blogs.js
@@ -1,23 +1,33 @@
 const blogsRouter = require('express').Router()
+const { compareSync } = require('bcrypt')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({}).populate('user')
+
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const oneUser = await User.findOne({})
+
     const newBlog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: oneUser._id
     })
 
-    const result = await newBlog.save()
-    response.status(201).json(result)
+    const savedBlog = await newBlog.save()
+    oneUser.blogs = (oneUser.blogs.concat(savedBlog._id))
+    await oneUser.save()
+
+    response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
--- part4/bloglist/bloglist-backend/tests/bloglist_api.test.js
+++ part4/bloglist/bloglist-backend/tests/bloglist_api.test.js
@@ -3,6 +3,8 @@ const supertest = require('supertest')
 const Blog = require('../models/blog')
 const helper = require('./test_helper')
 const app = require('../app')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
@@ -140,6 +142,79 @@ test('modification of a note succeeds', async () => {
     expect(blogToUpdateEnd.url).toBe(blogToUpdate.url)
     expect(blogToUpdateEnd.likes).toBe(70)
 }, 100000)
+
+describe.only('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    test('creation fails with proper statuscode and message if username is missing', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            name: 'spongebob squarepants',
+            password: 'frycook'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+
+        expect(result.body.error).toBe('username and/or password missing')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation fails if password is not long enough', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'patrick',
+            name: 'patrick star',
+            password: 'hi'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+
+        expect(result.body.error).toBe('password must contain at least 3 characters')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+})
+
 
 afterAll(() => {
     mongoose.connection.close()
