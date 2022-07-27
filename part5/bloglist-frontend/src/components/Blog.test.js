0397c23fcfeb66ff3bbb1399b63e11c67db11890
javier <jjr.agnir@gmail.com>
2022-07-27 11:06:59 +0800

    finish 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,22 +4,35 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author, but not url or likes by default', () => {
-    const blog = {
-        title: 'Component testing is done with react-testing-library',
-        author: 'Matti Lukkainen',
-        url: 'www.fullstackopen.com',
-        likes: '0',
-        user: '62d91318f759a24adb3535f2'
-    }
+describe('<Blog />' , () => {
+    let container
 
-    const { container } = render(<Blog blog={blog} />)
+    beforeEach(() => {
+        const blog = {
+            title: 'Component testing is done with react-testing-library',
+            author: 'Matti Lukkainen',
+            url: 'www.fullstackopen.com',
+            likes: '0',
+            user: '62d91318f759a24adb3535f2'
+        }
 
-    screen.debug()
+        container = render(<Blog blog={blog} />).container
+    })
 
-    const title = screen.getByText('Component testing is done with react-testing-library Matti Lukkainen')
-    expect(title).toBeDefined
+    test('renders title and author, but not url or likes by default', () => {
+        const title = screen.getByText('Component testing is done with react-testing-library Matti Lukkainen')
+        expect(title).toBeDefined
 
-    const div = container.querySelector('.togglableContent')
-    expect(div).toHaveStyle('display: none')
+        const div = container.querySelector('.togglableContent')
+        expect(div).toHaveStyle('display: none')
+    })
+
+    test('renders url and likes when button toggling details is clicked', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        const div = container.querySelector('.togglableContent')
+        expect(div).not.toHaveStyle('display: none')
+    })
 })
