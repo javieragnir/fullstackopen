7fb79b66cd13e9c7973ffa2180079a9e7c51f26d
javier <jjr.agnir@gmail.com>
2022-08-16 10:23:13 +0800

    remove Blog expansion functionality on Blog list page

--- part7/bloglist/bloglist-frontend/src/components/Blog.js
+++ part7/bloglist/bloglist-frontend/src/components/Blog.js
@@ -1,20 +1,6 @@
-import { useState } from 'react'
-import { likeBlog, deleteBlog } from '../reducers/blogReducer'
-import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Blog = ({ blog }) => {
-  const [blogVisible, setBlogVisible] = useState(false)
-
-  const dispatch = useDispatch()
-
-  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
-  const showWhenVisible = { display: blogVisible ? '' : 'none' }
-
-  const toggleBlogVisibility = () => {
-    setBlogVisible(!blogVisible)
-  }
-
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -23,46 +9,9 @@ const Blog = ({ blog }) => {
     marginBottom: 5,
   }
 
-  const updateLikes = (event) => {
-    event.preventDefault()
-    dispatch(likeBlog(blog))
-  }
-
-  const deleteSelf = (event) => {
-    event.preventDefault()
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)){
-      dispatch(deleteBlog(blog))
-    }
-  }
-
   return (
     <div className="blog" style={blogStyle}>
       <Link to={`/blogs/${blog.id}`}>{blog.title} {blog.author}</Link>
-      <button
-        className="viewButton"
-        onClick={toggleBlogVisibility}
-        style={hideWhenVisible}
-      >
-        view
-      </button>
-      <button
-        className="hideButton"
-        onClick={toggleBlogVisibility}
-        style={showWhenVisible}
-      >
-        hide
-      </button>
-      <div style={showWhenVisible} className="togglableContent">
-        <div className="url">{blog.url}</div>
-        <div className="likes">
-          likes {blog.likes}{' '}
-          <button onClick={updateLikes} className="likeButton">
-            like
-          </button>
-        </div>
-        <div>{blog.user.name}</div>
-      </div>
-      <button onClick={deleteSelf}>delete</button>
     </div>
   )
 }
