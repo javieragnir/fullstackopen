f7a801cabe40c8dcd84d587b0f4ac0c141e9782d
javier <jjr.agnir@gmail.com>
2022-08-16 08:55:52 +0800

    fix blog page not rendering

--- part7/bloglist/bloglist-frontend/src/components/BlogList.js
+++ part7/bloglist/bloglist-frontend/src/components/BlogList.js
@@ -1,36 +1,21 @@
 import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import Blog from './Blog'
 import Togglable from './Togglable'
 import BlogForm from './BlogForm'
-import { setUser } from '../reducers/userReducer'
 
 const BlogList = () => {
   const [formVisible, setFormVisible] = useState(false)
 
   const blogs = useSelector(state => state.blogs)
-  const user = useSelector(state => state.user)
-
-  const dispatch = useDispatch()
 
   const toggleVisibility = () => {
     setFormVisible(!formVisible)
   }
 
-  const handleLogout = (event) => {
-    event.preventDefault()
-
-    window.localStorage.removeItem('loggedBloglistUser')
-    dispatch(setUser(null))
-  }
-
   return (
     <div>
       <h2>blogs</h2>
-      <p>
-        {user.username} logged in
-        <button onClick={handleLogout}>logout</button>
-      </p>
       {blogs.map((blog) => (
         <Blog
           key={blog.id}
